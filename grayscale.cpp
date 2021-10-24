2739f06f907c22fb276b2e8fcd4abc7164ae5249
TeamElOmaryEssaiouad <86804928+TeamElOmaryEssaiouad@users.noreply.github.com>
2021-10-24 18:51:59 +0200

    Add files via upload

---
+++ grayscale.cpp
@@ -0,0 +1,25 @@
+#include "grayscale.h"
+
+Grayscale::Grayscale(string filename):PNG(){
+    readFromFile(filename);
+
+      for (unsigned i = 0; i <width(); i++) {
+        for (unsigned j = 0; j < height(); j++) {
+          HSLAPixel &P = getPixel(i, j);
+
+         P.s = 0;
+        }
+      }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
